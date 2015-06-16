0c35b91a5cc3e7b03deba78b965833fec0b88615
cpojer <christoph.pojer@gmail.com>
2015-06-16 16:06:19 -0700

    Add filters to rm-copyProperties.

--- transforms/rm-copyProperties.js
+++ transforms/rm-copyProperties.js
@@ -13,6 +13,18 @@ function rmCopyProperties(file, api, options) {
       argument => argument.type == 'ObjectExpression'
     );
 
+  const availableFilters = {
+    onlyPrototypeAssignments(path) {
+      var node = path.value.arguments[0];
+      return (
+        node.type == 'MemberExpression' &&
+        node.object.type == 'Identifier' &&
+        node.property.type == 'Identifier' &&
+        node.property.name == 'prototype'
+      );
+    }
+  };
+
   const rmCopyPropertyCalls = path =>
     j(path).replaceWith(j.callExpression(
       j.memberExpression(
@@ -23,12 +35,17 @@ function rmCopyProperties(file, api, options) {
       path.value.arguments
     ));
 
+  const filters = [
+    onlyObjectLiterals,
+    ...(options.filters || []).map(filterName => availableFilters[filterName])
+  ];
+
   const declarator = getRequireCall(root, 'copyProperties');
   if (declarator) {
     const variableName = declarator.value.id.name;
     const didTransform = root
       .find(j.CallExpression, {callee: {name: variableName}})
-      .filter(onlyObjectLiterals)
+      .filter(p => filters.every(filter => filter(p)))
       .forEach(rmCopyPropertyCalls)
       .size() > 0;
     if (didTransform) {
