d68c009fcd5e6d6294af639cd513ebe69fcfef6f
Andrew Hughson <andrewh@olympic.co.nz>
2016-01-11 17:17:51 +1300

    Add unquote-properties transform

---
+++ test/unquote-properties-test.js
@@ -0,0 +1,13 @@
+var x = {
+  'quotedProp': 1,
+  unquotedProp: 2,
+  'quoted-prop': 3,
+  method() { return 4; },
+  '_quotedProp': 5,
+  '$quotedProp': 6,
+  'ĦĔĽĻŎ': 7,
+  'quoted prop': 8,
+  'class': 9,
+  1: 10,
+  '2': 11,
+};
