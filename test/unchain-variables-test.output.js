b1d29593bd95ab0062ea3a1e84efb861f62b5ee3
Joe Lencioni <joe.lencioni@airbnb.com>
2016-03-01 16:06:15 -0800

    Add unchain-variables transform
    This transform will unchain chained variable declarations. Converting
    code like:
    
      var foo, bar;
    
    to:
    
      var foo;
      var bar;

---
+++ test/unchain-variables-test.output.js
@@ -0,0 +1,12 @@
+/* eslint-disable one-var, prefer-const */
+var foo = true;
+
+// A comment
+var bar = false;
+
+const baz = 1;
+const fiz = '2';
+let buzz = 3.3;
+let biz = {};
+var hello;
+var hi;
---
+++ transforms/unchain-variables.js
@@ -0,0 +1,35 @@
+/**
+ * Unchains chained variable declarations.
+ */
+module.exports = function(file, api) {
+  const jscodeshift = api.jscodeshift;
+
+  const chainedDeclarations = jscodeshift(file.source)
+    .find(jscodeshift.VariableDeclaration)
+    .filter(variableDeclaration => (
+      variableDeclaration.value.declarations.length > 1
+    ));
+
+  chainedDeclarations.forEach(chainedDeclaration => {
+    const kind = chainedDeclaration.value.kind; // e.g. const, let, or var
+
+    jscodeshift(chainedDeclaration)
+      .replaceWith(chainedDeclaration.value.declarations.map((declaration, i) => {
+        const unchainedDeclaration =
+          jscodeshift.variableDeclaration(kind, [declaration]);
+
+        if (i === 0) {
+          unchainedDeclaration.comments = chainedDeclaration.value.comments;
+        } else if (declaration.comments) {
+          unchainedDeclaration.comments = declaration.comments;
+          declaration.comments = null;
+        }
+
+        return unchainedDeclaration;
+      }));
+  });
+
+  return chainedDeclarations.size
+    ? chainedDeclarations.toSource({quote: 'single'})
+    : null;
+};
