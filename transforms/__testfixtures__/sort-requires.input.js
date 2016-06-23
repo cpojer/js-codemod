da8f5a6241e5db69a089998723f9bada8a5efc3a
Sergio Crisostomo <sergiosbox@gmail.com>
2016-06-23 11:44:35 +0100

    add requires alphabetic sort
    https://astexplorer.net/#/HPwPYV4rcI/6

---
+++ transforms/__testfixtures__/sort-requires.input.js
@@ -0,0 +1,11 @@
+"use strict";
+var ddd = require('../foo/bar');
+var ggg = require('ggg')(ccc);
+var aaa = require('aaa')(eee);
+var eee = require('eee');
+var ccc = require('ccc');
+var bbb = require('bbb');
+var foo = bbb.doSomething();
+var fff = require('fff');
+
+let restOfCode = 'foobaz';
---
+++ transforms/__testfixtures__/sort-requires.output.js
@@ -0,0 +1,11 @@
+"use strict";
+var bbb = require('bbb');
+var ccc = require('ccc');
+var ddd = require('../foo/bar');
+var eee = require('eee');
+var fff = require('fff');
+var aaa = require('aaa')(eee);
+var ggg = require('ggg')(ccc);
+var foo = bbb.doSomething();
+
+let restOfCode = 'foobaz';
