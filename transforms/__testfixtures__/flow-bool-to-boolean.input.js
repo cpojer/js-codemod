372e86f1733f8cdbaf393ac804fb1ef2891091a4
Christopher Chedeau <vjeuxx@gmail.com>
2016-09-04 10:56:12 -0700

    Flow bool -> boolean (#55)
    It turns out that there's no difference between those two at the AST level. So I just replace it by the same thing which is going make it dirty and reprinted by recast with the correct one.

---
+++ transforms/__testfixtures__/flow-bool-to-boolean.input.js
@@ -0,0 +1,7 @@
+/* @flow */
+
+var a: bool = 10;
+var b: boolean = 10;
+function c<T: bool>(x: bool): bool {
+  
+}
---
+++ transforms/__tests__/flow-bool-to-boolean-test.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const defineTest = require('jscodeshift/dist/testUtils').defineTest;
+defineTest(__dirname, 'flow-bool-to-boolean', null, 'flow-bool-to-boolean');
