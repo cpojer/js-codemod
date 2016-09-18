068118503d379b31a7df3a8bc145e028aff79af8
Christopher Chedeau <vjeuxx@gmail.com>
2016-09-18 16:11:19 -0700

    Update unchain-variables.js
    size is a function :) cc @lencioni

--- transforms/unchain-variables.js
+++ transforms/unchain-variables.js
@@ -33,7 +33,7 @@ module.exports = function(file, api, options) {
       }));
   });
 
-  return chainedDeclarations.size
+  return chainedDeclarations.size()
     ? chainedDeclarations.toSource(printOptions)
     : null;
 };
