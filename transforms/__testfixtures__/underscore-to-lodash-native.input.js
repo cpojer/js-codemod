7de29c86a24b94255452fc00093e118f4a04e5e3
Zack Argyle <zackargyle@pinterest.com>
2016-09-21 10:06:43 -0700

    Add underscore-to-lodash-native transform

---
+++ transforms/__testfixtures__/underscore-to-lodash-native.input.js
@@ -0,0 +1,5 @@
+// Test comment
+const _ = require('underscore');
+_.forEach([1, 2], num => num);
+const test = [1, 2, 3];
+const result = _.find(test, 2);
---
+++ transforms/__testfixtures__/underscore-to-lodash-native.output.js
@@ -0,0 +1,5 @@
+// Test comment
+import {find} from 'lodash';
+[1, 2].forEach(num => num);
+const test = [1, 2, 3];
+const result = find(test, 2);
